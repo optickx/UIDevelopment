a5ee9a0c90d2461bf81946b56c0e6da6cd4bd90e
optickx <danielbarrios2002@gmail.com>
2023-02-09 18:18:54 +0100

    fifth commit

--- src/app/App.java
+++ src/app/App.java
@@ -1,7 +1,34 @@
 package app;
 
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import ui.control.CustomerDataController;
+
+
+public class App extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader loader = 
+                new FXMLLoader(getClass().getClassLoader().getResource("ui/view/CustomerData.fxml"));
+            Parent root = (Parent) loader.load();
+            //Obtain the Sign In window controller
+            CustomerDataController controller = CustomerDataController.class.cast(loader.getController());
+
+            controller.setStage(stage);
+            controller.initStage(root);
+        } catch (IOException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
